aa0d23e86f52c16f119311a748a65258fface509
slopezmazo <141847950+slopezmazo@users.noreply.github.com>
2023-08-29 23:51:26 -0500

    bug fixed Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Environment variable:"+System.getenv("variableName"));
+        System.out.println("Environment variable:"+System.getenv("ENVIRONMENT_VAR"));
     }
 }
